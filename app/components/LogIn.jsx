a4d5c48ea2dc38f5630ab09b16fc14721ee7c8cc
Francy Lang <francy.lang@gmail.com>
2017-10-17 20:34:34 -0600

    Started constructing Login.jsx file

--- app/components/LogIn.jsx
+++ app/components/LogIn.jsx
@@ -1,1 +1,36 @@
 import React, { Component } from 'react';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <form>
+          <h2>Welcome to MovieTracker</h2>
+          <h2>LOGIN</h2>
+          <input
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            className="form-button"
+            type="submit"
+          />
+        </form>
+      </div>
+    );
+  }
+}
