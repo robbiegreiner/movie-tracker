5446d8fda1e3394a0744cca49a6136d030b0076d
Lola Brenner <amandadbrenner@gmail.com>
2017-10-19 16:37:47 -0600

    deletes fetch helper test and fixes container test thanks to limbo

--- app/helpers/movieDataFetcher.js
+++ app/helpers/movieDataFetcher.js
@@ -6,8 +6,8 @@ const movieDataFetcher = () => {
   return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US`)
     .then(response => response.json())
     // .then(response => console.log(response))
-    // .then(responseJSON => responseJSON.results)
-    // .then(moviesArray => MovieDataCleaner(moviesArray));
+    .then(responseJSON => responseJSON.results)
+    .then(moviesArray => MovieDataCleaner(moviesArray));
 };
 
 export default movieDataFetcher;
