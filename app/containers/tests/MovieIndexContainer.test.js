cbf627f7e0113dd7066d9b560ba1c0a91778d980
Lola Brenner <amandadbrenner@gmail.com>
2017-10-19 16:01:14 -0600

    so lost not sure what this commit does

---
+++ app/containers/tests/MovieIndexContainer.test.js
@@ -0,0 +1,27 @@
+import configureStore from 'redux-mock-store';
+import { shallow, mount, configure } from 'enzyme';
+import { mapStateToProps, mapDispatchToProps } from '../MovieIndexContainer';
+import MovieIndexContainer from '../MovieIndexContainer';
+import MovieIndex from '../../components/movieIndex';
+import React from 'react';
+import * as actions from '../../actions';
+import thunk from 'redux-thunk';
+
+describe('MovieIndexContainer', () => {
+  const middlewares = [thunk];
+  const mockStore = configureStore();
+  const mockFn = jest.fn();
+  const initialState = { movieList: [] };
+  const store = mockStore(initialState, middlewares);
+  const wrapper = mount(<MovieIndexContainer
+    store={store}
+    movieList={initialState}
+    retrieveMovies={mockFn}
+  />);
+
+  it('should have default state', () => {
+    // console.log(wrapper.instance().props.movieList);
+
+    expect(wrapper.instance().props.movieList).toEqual({ movieList: [] });
+  });
+});
--- app/helpers/tests/movieDataFetcher.test.js
+++ app/helpers/tests/movieDataFetcher.test.js
@@ -16,23 +16,31 @@ describe('movieDataFetcher', () => {
     });
   };
 
+  const delay = () => new Promise((res, rej) => setTimeout(res, 3000));
+
   beforeEach( async () => {
-    fetchMock.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US`, {
+    fetchMock.mock(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US`, {
       status: 200,
       body: mockMovieData
     });
   });
 
   it('should return the array from the results key', async () => {
+
+    fetchMock.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US`, delay().then(() => ({
+      status: 200,
+      body: mockMovieData
+    })));
+    await pause();
     const movieDataArray = movieDataFetcher();
 
     await pause();
-    await pause();
 
-    console.log();
+    console.log(movieDataArray);
+    // console.log(mockMovieData);
 
     // expect(movieDataArray).toEqual()
 
-    // expect(movieDataArray).toEqual(movieDataCleaner(mockMovieData.results));
+    expect(movieDataArray).toEqual(mockMovieData);
   });
 });
