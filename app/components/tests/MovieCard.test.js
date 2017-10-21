10685c77e7f85211b34924fe8e3688d0fd7cfc3b
Lola Brenner <amandadbrenner@gmail.com>
2017-10-21 10:53:27 -0600

    begins card test

---
+++ app/components/tests/MovieCard.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MovieCard from '../MovieCard';
+import { shallow } from "enzyme";
+import mockMovieData from '../../helpers/mockMovieData';
+
+describe('MovieCard', () => {
+  const movie = mockMovieData.results[0];
+  const wrapper = shallow(<MovieCard
+    title={movie.title}
+    movie_id={movie.id}
+    releaseDate={movie.release_date}
+    overview={movie.overview}
+    vote_average={movie.vote_average}
+    poster_path={movie.poster_path}
+    addFavorites={() => { jest.fn(); }} />);
+
+  it('should render a card', () => {
+    const card = wrapper.find('.movie-card');
+
+    expect(card.length).toEqual(1);  
+  });
+});
