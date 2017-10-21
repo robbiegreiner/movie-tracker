eed248676babed7580970ee9c39f62990fdce94f
Lola Brenner <amandadbrenner@gmail.com>
2017-10-21 11:25:52 -0600

    finishes card test

--- app/components/tests/MovieCard.test.js
+++ app/components/tests/MovieCard.test.js
@@ -3,6 +3,8 @@ import MovieCard from '../MovieCard';
 import { shallow } from "enzyme";
 import mockMovieData from '../../helpers/mockMovieData';
 
+//will need a test for favorites button when we get done with iter 2
+
 describe('MovieCard', () => {
   const movie = mockMovieData.results[0];
   const wrapper = shallow(<MovieCard
@@ -24,5 +26,31 @@ describe('MovieCard', () => {
     const img = wrapper.find('img');
 
     expect(img.length).toEqual(1);
+    expect(img.html().includes(movie.poster_path)).toEqual(true);
+  });
+
+  it('should render a release date', () => {
+    const release = wrapper.find('.release');
+
+    expect(release.length).toEqual(1);
+    expect(release.text()).toEqual(movie.release_date);
+  });
+
+  it('should render a score', () => {
+    const score = wrapper.find('.score');
+
+    expect(score.length).toEqual(1);
+    expect(score.text()).toEqual(`${movie.vote_average}`);
+  });
+
+  it('should render an overview', () => {
+    const overview = wrapper.find('p');
+
+    expect(overview.length).toEqual(1);
+    expect(overview.text()).toEqual(movie.overview);
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
