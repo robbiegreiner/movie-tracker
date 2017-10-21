4aab7165a315f303750381d755f4bf2f40e99400
Lola Brenner <amandadbrenner@gmail.com>
2017-10-21 14:27:11 -0600

    fixes some tests

--- app/components/tests/Header.test.js
+++ app/components/tests/Header.test.js
@@ -14,20 +14,19 @@ describe('Header component', () => {
   it('should render user name when user is signed in', () => {
     const name = wrapper.find('p');
 
-    expect(name.text()).toEqual('Lola');
+    expect(name.text()).toEqual('Welcome, Lola!');
   });
 
   it('should render a link to view favorites', () => {
     const link = wrapper.find('Link');
 
-    expect(link.length).toEqual(1);
+    expect(link.length).toEqual(2);
   });
 
   it('should render a button to sign out', () => {
-    const button = wrapper.find('button');
+    const button = wrapper.find('.signout');
 
     expect(button.length).toEqual(1);
-    expect(button.text()).toEqual('Sign Out');
   });
 
   it('should match snapshot', () => {
--- app/reducers/createUserError.js
+++ app/reducers/createUserError.js
@@ -1,9 +1,14 @@
 const createUserError = (state = false, action) => {
   switch (action.type) {
+<<<<<<< HEAD
   case 'CREATE_USER_ERROR':
     return action.createUserError;
   case 'FETCH_USER_SUCCESS':
     return false;
+=======
+  case ('CREATE_USER_ERROR') :
+    return true;
+>>>>>>> fixes some tests
   default:
     return state;
   }
