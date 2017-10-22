f610a165e1e70e7bf502181e6a66e502faa17218
Lola Brenner <amandadbrenner@gmail.com>
2017-10-22 14:45:58 -0600

    refactors all fetches into helpers

--- app/helpers/deleteFaveFetcher.js
+++ app/helpers/deleteFaveFetcher.js
@@ -1,5 +1,11 @@
-const deleteFaveFetcher = () => {
-
+const deleteFaveFetcher = (userId, movie) => {
+  fetch(`api/users/${userId}/favorites/${movie.movie_id}`, {
+    method: 'DELETE',
+    body: JSON.stringify([userId, movie.movie_id]),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 };
 
 export default deleteFaveFetcher;
