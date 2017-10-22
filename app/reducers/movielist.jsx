40e63f9be724caaab656cab66ee881c7dd2024aa
Robbie Greiner <robbieg@gmail.com>
2017-10-22 13:40:05 -0600

    added to reducer so when a movie is unfavorited it it will movieList isFav to false

--- app/reducers/movielist.jsx
+++ app/reducers/movielist.jsx
@@ -3,7 +3,7 @@ const movieList = (state = [], action) => {
   case "FETCH_DATA_SUCCESS":
     return action.movies.map( movie => {
       //if movie is in faves list, isFave: true
-      //we don't have access to faves list here 
+      //we don't have access to faves list here
       return Object.assign({}, movie, { isFav: false });
     });
   case 'ADD_FAVORITE':
@@ -13,6 +13,15 @@ const movieList = (state = [], action) => {
       }
       return movie;
     });
+  case 'DELETE_FAVORITE':
+    return state.map( stateMovie => {
+      if ( action.movie.title === stateMovie.title ) {
+        return Object.assign({}, stateMovie, action.movie, { isFav: false });
+      }
+      return stateMovie;
+    });
+
+
   // case 'FETCH_DATA_SUCCESS':
   //   return action.movies;
   default:
